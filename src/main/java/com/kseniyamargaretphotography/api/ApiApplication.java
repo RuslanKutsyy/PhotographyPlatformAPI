cbc808341626d969f6f8d6fcadad6e668b72bfae
rkutsyy <rkutsyy@peoplestrategy.com>
2021-10-11 22:12:02 +0300

    Project Structure Update

--- src/main/java/com/kseniyamargaretphotography/api/ApiApplication.java
+++ src/main/java/com/kseniyamargaretphotography/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.photographyplatform.api;
+package com.kseniyamargaretphotography.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
